a5b41ea0742aa59e8ac71ed080f75aa2bba1332d
Mica Swyers <mswyers@twilio.com>
2019-07-24 15:02:06 -0700

    Prettier-fy chat.js file

--- src/commands/token/chat.js
+++ src/commands/token/chat.js
@@ -1,6 +1,6 @@
 const { flags } = require('@oclif/command');
 const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
-const { TwilioCliError} = require('@twilio/cli-core').services.error;
+const { TwilioCliError } = require('@twilio/cli-core').services.error;
 const Twilio = require('twilio');
 
 class ChatTokenGenerator extends TwilioClientCommand {
@@ -10,36 +10,40 @@ class ChatTokenGenerator extends TwilioClientCommand {
     this.showHeaders = true;
   }
 
-async runCommand() {
-  this.logger.info("Copy/paste this chat token into your test application:");
-  const accessToken = new Twilio.jwt.AccessToken(
-  	this.twilioClient.accountSid,
-  	this.twilioClient.username,
-  	this.twilioClient.password
-  );
+  async runCommand() {
+    this.logger.info('Copy/paste this chat token into your test application:');
+    const accessToken = new Twilio.jwt.AccessToken(
+      this.twilioClient.accountSid,
+      this.twilioClient.username,
+      this.twilioClient.password
+    );
 
-  let chatGrant = new Twilio.jwt.AccessToken.ChatGrant({
-  	serviceSid: this.flags["chat-service-sid"]
-  });
+    let chatGrant = new Twilio.jwt.AccessToken.ChatGrant({
+      serviceSid: this.flags['chat-service-sid'],
+    });
 
-  accessToken.addGrant(chatGrant);
-  accessToken.identity = this.flags["identity"];
+    accessToken.addGrant(chatGrant);
+    accessToken.identity = this.flags['identity'];
 
-  this.output({jwt: accessToken.toJwt()},undefined,{showHeaders: false});
-
-}
+    this.output({ jwt: accessToken.toJwt() }, undefined, {
+      showHeaders: false,
+    });
+  }
 }
 
 const ChatTokenGeneratorFlags = {
-	'identity': flags.string({
-		description: 'The user identity for this Chat',
-		required: true
-	}),
-	'chat-service-sid': flags.string({
-		description: 'The service SID for the Chat, starts with ISXXX',
-		required: true
-	})
+  identity: flags.string({
+    description: 'The user identity for this Chat',
+    required: true,
+  }),
+  'chat-service-sid': flags.string({
+    description: 'The service SID for the Chat, starts with ISXXX',
+    required: true,
+  }),
 };
 
-ChatTokenGenerator.flags = Object.assign(ChatTokenGeneratorFlags, TwilioClientCommand.flags);
+ChatTokenGenerator.flags = Object.assign(
+  ChatTokenGeneratorFlags,
+  TwilioClientCommand.flags
+);
 module.exports = ChatTokenGenerator;
