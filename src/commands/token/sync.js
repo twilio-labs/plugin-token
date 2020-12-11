9276fe12577a0c0afd025e80a4b0dd2c0c8c4bac
Jeff Linwood <jlinwood@gmail.com>
2020-12-11 13:28:41 -0600

    Add sync token support (#20)
    * Add support for Sync access tokens
    
    * Update README for Sync Token docs

---
+++ src/commands/token/sync.js
@@ -0,0 +1,61 @@
+const { flags } = require('@oclif/command');
+const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
+const Twilio = require('twilio');
+const createToken = require('../../helpers/accessToken.js');
+const globalFlags = require('../../helpers/globalFlags.js');
+
+class SyncTokenGenerator extends TwilioClientCommand {
+  constructor(argv, config, secureStorage) {
+    super(argv, config, secureStorage);
+
+    this.showHeaders = true;
+  }
+
+  validateSyncServiceSid() {
+    return (
+      this.flags['sync-service-sid'].startsWith('IS') &&
+      this.flags['sync-service-sid'].length === 34
+    );
+  }
+
+  async run() {
+    await super.run();
+
+    const accessToken = createToken.call(this);
+
+    if (!this.validateSyncServiceSid()) {
+      this.logger.error(
+        'Invalid Sync Service SID, must look like ISxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+      );
+      process.exit(1);
+    }
+
+    let syncGrant = new Twilio.jwt.AccessToken.SyncGrant({
+      serviceSid: this.flags['sync-service-sid'],
+    });
+    accessToken.addGrant(syncGrant);
+
+    this.logger.info('Copy/paste this sync token into your test application:');
+    this.output({ jwt: accessToken.toJwt() }, undefined, {
+      showHeaders: false,
+    });
+  }
+}
+
+const SyncTokenGeneratorFlags = {
+  identity: flags.string({
+    description: 'The user identity for this Sync Service',
+    required: true,
+  }),
+  'sync-service-sid': flags.string({
+    description: 'The service SID for the Sync, starts with ISXXX',
+    required: true,
+  }),
+};
+
+SyncTokenGenerator.flags = Object.assign(
+  SyncTokenGeneratorFlags,
+  TwilioClientCommand.flags,
+  globalFlags
+);
+module.exports = SyncTokenGenerator;
